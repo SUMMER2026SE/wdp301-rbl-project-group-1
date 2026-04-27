e149e9ac8f6fdbecd5f831ac16cf955ea5c512ef
Anh Tú <anhtunguyen643@gmail.com>
2026-04-27 06:01:59 +0000

    feat(EDU-45): completed core business database

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { IUserRepository } from './domain/repositories/user.repository.interface';
-import { IProfileRepository } from './domain/repositories/profile.repository.interface';
-import { PrismaUserRepository } from './infrastructure/repositories/user.repository.impl';
-import { PrismaProfileRepository } from './infrastructure/repositories/profile.repository.impl';
-import { GetUsersQueryHandler } from './application/queries/get-users/get-users.handler';
-import { UpdateProfileHandler } from './application/commands/update-profile/update-profile.handler';
+
 import { ChangeAvatarHandler } from './application/commands/change-avatar/change-avatar.handler';
 import { UpgradeTutorCommandHandler } from './application/commands/upgrade-tutor/upgrade-tutor.handler';
+import { UpdateProfileHandler } from './application/commands/update-profile/update-profile.handler';
+
+import { GetUsersQueryHandler } from './application/queries/get-users/get-users.handler';
+
+import { IProfileRepository } from './domain/repositories/profile.repository.interface';
+import { IUserRepository } from './domain/repositories/user.repository.interface';
+
+import { PrismaProfileRepository } from './infrastructure/repositories/profile.repository.impl';
+import { PrismaUserRepository } from './infrastructure/repositories/user.repository.impl';
+
 import { UserController } from './presentation/controllers/user.controller';
 
 @Module({
