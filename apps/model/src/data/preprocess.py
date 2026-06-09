fba2e2274c193f5e8fb46c063f9754443533e6cc
tudeptrai <anhtunguyen643@gmail.com>
2026-06-09 13:30:42 +0700

    chore: remove old model subtree

--- apps/model/src/data/preprocess.py
+++
@@ -1,6 +0,0 @@
-from src.config.settings import EVENT_WEIGHTS
-
-def add_score(df):
-    processed = df.copy()
-    processed["score"] = processed["event_type"].map(EVENT_WEIGHTS).fillna(1)
-    return processed
